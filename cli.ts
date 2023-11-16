8944040f2a788da7dfc66db99f8267ead8005582
Cory Gross <coryg89@gmail.com>
2023-11-15 20:21:51 -0800

    Minor updates to cli logic, more verbose messages, print usage

--- cli.ts
+++ cli.ts
@@ -5,7 +5,7 @@ import { promptSecret } from './helpers/prompt-secret.ts';
 import { login, devices, status } from './mod.ts';
 
 const [command, ...restArgs] = Deno.args;
-const args = parse(restArgs, { string: ['account', 'device', 'value'] });
+const args = parse(restArgs, { string: ['account', 'device', 'value'], boolean: ['help'] });
 
 const cacheDir = Deno.env.get('XDG_CACHE_HOME') || `${Deno.env.get('HOME') || '~'}/.cache`;
 const moduleCacheDir = `${cacheDir}/deno-vesync`;
@@ -13,6 +13,14 @@ const moduleCacheFile = `${moduleCacheDir}/config.json`;
 
 const encoder = new TextEncoder();
 const decoder = new TextDecoder('utf-8');
+
+function printUsage() {
+    Deno.stderr.write(encoder.encode(
+        '\nUsage:\n\nAccount Login)\n\n\tvesync-cli login --account <account>'
+        + '\n\nDevice Listing)\n\n\tvesync-cli devices'
+        + '\n\nUpdate Device)\n\n\tvesync-cli status --device <device> --value <on|off>\n\n'
+    ));
+}
 
 async function readConfig() {
     let config
@@ -31,10 +39,17 @@ function writeConfig(config: { account: string, password: string, devices?: Arra
         .then(() => Deno.writeFile(moduleCacheFile, encoder.encode(configString)));
 }
 
+if (args.help) {
+    printUsage();
+    Deno.exit(1);
+}
+
 if (/^login$/i.test(command)) {
     const { account } = args;
     if (!account) {
-        console.error('Must pass arg --account with VeSync account email');
+        Deno.stderr.write(encoder.encode('Error: Must pass argument --account with VeSync account email when logging in'));
+        printUsage();
+        Deno.exit(1);
     }
     const password = await promptSecret('Please enter your password: ');
 
@@ -47,7 +62,7 @@ if (/^login$/i.test(command)) {
     const res = await login(account, hash);
     const { accountID, tk } = await res.json();
     if (!accountID || !tk || typeof accountID !== 'string' || typeof tk !== 'string') {
-        console.error(`Could not get account id/token from VeSync API for account ${account}`);
+        Deno.stderr.write(encoder.encode(`Error: Could not get account id/token from VeSync API for account ${account}`));
         Deno.exit(1);
     }
 
@@ -65,11 +80,13 @@ if (/^login$/i.test(command)) {
 } else if (/^(status)$/i.test(command)) {
     const { device, value } = args;
     if (!device) {
-        console.error('Must pass arg --device with VeSync device name');
+        Deno.stderr.write(encoder.encode('Error: Must pass argument --device with VeSync device name to update device status'));
+        printUsage();
         Deno.exit(1);
     }
     if (!value || !/^(on|off)$/.test(value.toLowerCase())) {
-        console.error('Must pass arg --value as one of \'on\' or \'off\'');
+        Deno.stderr.write(encoder.encode('Error: Must pass argument --value as one of \'on\' or \'off\' to update device status'));
+        printUsage();
         Deno.exit(1);
     }
     const config = await readConfig();
@@ -82,21 +99,23 @@ if (/^login$/i.test(command)) {
         config.devices = await res.json();
         await writeConfig(config);
         await Deno.stderr.write(encoder.encode(`Device config successfully saved to local cache: ${moduleCacheFile}`));
-        
+
         ({ cid: deviceId, deviceType } = config.devices && config.devices.find(({ deviceName } : Device) => deviceName === device));
         if (!deviceId || !deviceType || typeof deviceId !== 'string' || typeof deviceType !== 'string') {
-            console.error(`Could not get device id/type from VeSync API for device ${device}`);
+            Deno.stderr.write(encoder.encode(`Error: Could not get device id/type from VeSync API for device ${device}`));
             Deno.exit(1);
-        }    
+        }
     }
 
     const deviceStatus = value.toLowerCase();
     const res = await status(accountID, tk, deviceType, deviceId, deviceStatus);
     if (res.ok) {
-        await Deno.stderr.write(encoder.encode(`Successfully updated device ${device} (${deviceId}) with new status: ${deviceStatus}`));
+        Deno.stderr.write(encoder.encode(`Successfully updated device ${device} (${deviceId}) with new status: ${deviceStatus}`));
     } else {
-        console.error(`Failed to update ${device} (${deviceId}) with new status: ${deviceStatus}`);
+        Deno.stderr.write(encoder.encode(`Error: Failed to update ${device} (${deviceId}) with new status: ${deviceStatus}`));
         Deno.exit(1);
     }
+} else {
+    printUsage();
+    Deno.exit(1);
 }
-
