e4f91e66be4eda22fa6823df43ac8f0068a270f6
Cory Gross <coryg89@gmail.com>
2023-11-15 20:20:48 -0800

    Update stdlib dependencies to 0.206.0 from 0.136.0

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
-export { parse } from 'https://deno.land/std@0.136.0/flags/mod.ts';
-export { crypto } from 'https://deno.land/std@0.136.0/crypto/mod.ts';
-export { ensureDir } from 'https://deno.land/std@0.136.0/fs/mod.ts';
+export { parse } from 'https://deno.land/std@0.206.0/flags/mod.ts';
+export { crypto } from 'https://deno.land/std@0.206.0/crypto/mod.ts';
+export { ensureDir } from 'https://deno.land/std@0.206.0/fs/mod.ts';
